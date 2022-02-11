4fc2f7b2f856c07cdcc9f8d336daaf181be2f0f1
Thainguyen3003 <nguyennangthai@gmail.com>
2022-02-11 15:46:46 +0700

    xong trang home

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@home')->name('home');
+Route::get('/trang-chu', 'HomeController@home')->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home'); */
